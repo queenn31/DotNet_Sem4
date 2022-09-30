78bad85d9176b36d82f68363bdd549693d3167fa
queenn31 <queenn31@gmail.com>
2022-09-30 19:18:19 +0300

    complit number 1

---
+++ Num1/Program.cs
@@ -0,0 +1,16 @@
+﻿int Exponentiation(int numberA, int numberB){
+  int result = 1;
+  for(int i=1; i <= numberB; i++){
+    result = result * numberA;
+  }
+    // int result = Math.Pow(numberA, numberB);
+    return result;
+}
+
+  Console.Write("Введите первое число: ");
+  int numberA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите второе число: ");
+  int numberB = Convert.ToInt32(Console.ReadLine());
+
+  int exponentiation = Exponentiation(numberA, numberB);
+  Console.WriteLine("Итог: " + exponentiation);
